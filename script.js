fc4a3c25d848939e9ef0dc64fe069f73c95b7a3a
ahmedUbaidulla <theubaidulla@gmail.com>
2026-03-12 20:46:05 +0100

    update organic chemistry tutorial

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-function hello(){
-  alert("Hello world!");
+
+const prefixes =
+{
+1:"meth",
+2:"eth",
+3:"prop",
+4:"but",
+5:"pent",
+6:"hex"
+};
+
+function generateName()
+{
+const carbons = document.getElementById("carbons").value;
+const type = document.getElementById("type").value;
+const position = document.getElementById("position").value;
+
+const prefix = prefixes[carbons];
+
+let name = "";
+
+if(type === "alkane")
+{
+name = prefix + "ane";
+}
+
+if(type === "alkene")
+{
+name = prefix + "-" + position + "-ene";
+}
+
+if(type === "alcohol")
+{
+name = prefix + "an-" + position + "-ol";
+}
+
+document.getElementById("result").innerText = name;
+}
+
+function checkAnswer()
+{
+const answer = document.getElementById("answer").value.toLowerCase();
+
+if(answer === "propane")
+{
+document.getElementById("quizResult").innerText = "Correct!";
+}
+else
+{
+document.getElementById("quizResult").innerText = "Try again.";
+}
 }
